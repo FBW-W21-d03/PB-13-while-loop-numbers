1138123d4863c5a310e0da0a17e15aaf329bc41a
kawanaza <kawanbabani@gmail.com>
2021-11-18 10:47:08 +0100

    done

---
+++ index.js
@@ -0,0 +1,14 @@
+
+let auto = 93;
+
+while ( auto <99){
+    console.log(auto);
+    auto=auto + 1 ;
+}
+
+let autosParkplatz = 100;
+
+while (auto >= autosParkplatz) {
+    console.log("Leide Keine Auto Parkplatz mehr.. Bitte Warten");
+    auto - autosParkplatz 
+} 
