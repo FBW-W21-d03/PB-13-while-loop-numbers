2b5bb097926a0069c3efb2b89eaf3314e08f1437
sprotte <metatron1972@live.de>
2021-11-18 09:43:17 +0100

    sprotte

---
+++ index.js
@@ -0,0 +1,15 @@
+/*
+let isopen = true;
+if (isopen) {console.log("Hallo");}
+else {console.log("ist voll")};
+*/
+/*
+const einkaufskorb = ["apfel", "birne"];
+while (einkaufskorb.length > 0) {let lastItem = einkaufskorb(einkaufskorb.length - 1);
+console.log("heraus nehmen", lastItem);
+console.log(einkaufskorb);}
+*/
+let spotsRemaning = 100;
+while(spotsRemaning > 0){console.log("Zahl");
+spotsRemaning = spotsRemaning -1;}
+
