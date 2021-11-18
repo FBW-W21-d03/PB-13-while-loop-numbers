db7f837e907aeb97e70f97761d45a8ac13cbafc2
Dennisbullys <d.rochus@hotmail.de>
2021-11-18 09:50:19 +0100

    Dennis

---
+++ Dennis.js
@@ -0,0 +1,26 @@
+console.log("while-loop-numbers");
+
+// Aufgabenstellung Teil 1
+
+let index = 0
+
+
+while (index < 100) {
+    console.log(index);
+    index ++;    
+}
+
+// Aufgabenstellung Teil 2
+// Erhöhe die Obergrenze nun auf 1000 und vergleiche dein Ergebnis
+
+let index2 = 0;
+
+while (index2 < 1000) {
+  console.log(index2);
+  index2 ++;
+}
+
+
+
+
+
