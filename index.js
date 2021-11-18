b07091f0c4e48e2a510facd82795bb55528eb8b0
aisha2000-hub <aisha.noori2000@gmail.com>
2021-11-18 10:01:04 +0100

    Die lösung von Aisha

---
+++ index.js
@@ -0,0 +1,16 @@
+// TEIL-1
+let zahl = 0;
+
+while (zahl<100) {
+    console.log(zahl);
+    zahl++;
+    
+}
+// TEIL-2
+let Zahl = 0;
+
+while (Zahl<1000) {
+    console.log(Zahl);
+    Zahl++;
+    
+}
