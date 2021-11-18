0258eb69a63d2a87962f37ed3228046c83ae9f9e
slavablack <veaceslav.ciornii@magenta.de>
2021-11-18 09:57:07 +0100

    schleife

---
+++ while.js
@@ -0,0 +1,15 @@
+//Teil 1
+let zahl=0;
+while (zahl<100) {
+    console.log(zahl);
+    zahl=zahl +1;
+    
+}
+
+//Teil 2
+let zahl2=0;
+while (zahl2<1000) {
+    console.log(zahl2);
+    zahl2=zahl2 +1;
+    
+}
