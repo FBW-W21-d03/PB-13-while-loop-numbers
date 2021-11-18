da60b2b373d54b5fb84bd1f691a6822a31e77bf7
Hayat Alkheder <hayat.alkheder@gmail.com>
2021-11-18 10:16:47 +0100

    js

---
+++ index.js
@@ -0,0 +1,12 @@
+// 1)
+let Aufgabenstellung1 = 0;
+while (Aufgabenstellung1 <100 ) {
+    console.log(Aufgabenstellung1);
+    Aufgabenstellung1=Aufgabenstellung1 +1;
+} 
+// 2 )
+let Aufgabenstellung2 =0;
+while (Aufgabenstellung2 < 1000) {
+   console.log(Aufgabenstellung2); 
+   Aufgabenstellung2=Aufgabenstellung2 +1;
+}
